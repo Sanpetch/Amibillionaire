5e3d298924297b72977e9bc9cece2e69863de31e
sanpetch <nick@sanpetchs-macbook-pro.local>
2018-10-29 01:54:28 +0700

    Initial Commit

---
+++ Amibillionaire/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Amibillionaire
+//
+//  Created by nick on 29/10/2561 BE.
+//  Copyright © 2561 nick. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
